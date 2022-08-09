1c9dbf61b63365989235d6a4ad34968c071f792f
KeftonDavid <keftondavid@gmail.com>
2022-08-09 19:09:42 -0300

    Adição do bootstrap e renderização com handlebars

--- blogapp/app.js
+++ blogapp/app.js
@@ -4,6 +4,8 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const admin = require('./routes/admin');
+const path = require('path');
 
 //Configurações
 //- Body Parser
@@ -17,9 +19,11 @@ app.set('view engine', 'handlebars');
 //- Mongoose
 
 
-//
+//- Public
+app.use(express.static(path.join(__dirname, "public")))
 
 //Rotas
+app.use('/admin', admin)
 
 //Outros
 const PORT = 8081;
--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Página principal do painel de administrador');
+    res.render('admin/index.handlebars')
 })
 
 router.get('/posts', (req, res) => {
