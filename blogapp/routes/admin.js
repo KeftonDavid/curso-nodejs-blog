e265bf38b821e8a112fec84a3dfcffd233d41127
KeftonDavid <keftondavid@gmail.com>
2022-08-13 22:58:32 -0300

    Criando formulario de postagens

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -121,4 +121,18 @@ router.post("/categorias/deletar", (req, res) => {
     })
 })
 
+router.get("/postagens", (req, res) => {
+    res.render("admin/postagens.handlebars")
+})
+
+router.get("/postagens/add", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("admin/addpostagem.handlebars", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário");
+        res.redirect("/admin");
+    })
+    
+})
+
 module.exports = router;
