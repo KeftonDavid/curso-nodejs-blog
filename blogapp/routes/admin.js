abb14b32a0db2e315908e5195fde3786caac506e
KeftonDavid <keftondavid@gmail.com>
2022-08-16 01:38:39 -0300

    Criação de edição de postagem: novas rotas get e post

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -169,4 +169,46 @@ router.post("/postagens/nova", (req, res) => {
     }
 })
 
+router.get("/postagens/edit/:id", (req, res) => {
+    
+    Postagem.findOne({_id: req.params.id}).lean().then((postagem) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("admin/editpostagens.handlebars", {categorias: categorias, postagem: postagem});
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar categorias");
+            res.redirect("/admin/postagens");
+        })
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao carregar o formulário de edição");
+        res.redirect("/admin/postagens")
+    })
+})
+
+router.post("/postagens/edit",(req, res) => {
+    Postagem.findOne({_id: req.body.id}).sort({data: "desc"}).then((postagem) => {
+        
+        postagem.titulo = req.body.titulo;
+        postagem.slug = req.body.slug;
+        postagem.descricao = req.body.descricao;
+        postagem.conteudo = req.body.conteudo;
+        postagem.categoria = req.body.categoria;
+
+        postagem.save().then(() => {
+        
+            req.flash("success_msg", "Postagem editada com sucesso")
+            res.redirect('/admin/postagens')
+        }).catch((err) => {
+            console.log(err);
+            req.flash("error_msg", "Erro interno.");
+            res.redirect("/admin/postagens")
+        })
+    }).catch((err) => {
+        console.log(err);
+        req.flash("error_msg", "Houve um erro ao salvar a edição.");
+        res.redirect("/admin/postagens");
+    })
+
+
+})
+
 module.exports = router;
