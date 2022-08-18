2d1350134644e6c6184cd21a926ffd16fcd4c36b
KeftonDavid <keftondavid@gmail.com>
2022-08-17 23:58:33 -0300

    Listagem de categorias: Mostrando postagens de acordo com a categoria selecionada

--- blogapp/app.js
+++ blogapp/app.js
@@ -10,6 +10,8 @@ const session = require('express-session');
 const flash = require('connect-flash');
 require("./models/Postagem");
 const Postagem = mongoose.model("postagens");
+require("./models/Categoria");
+const Categoria = mongoose.model("categorias");
 //Configurações
 //- Sessão
 app.use(session({
@@ -72,6 +74,35 @@ app.get("/postagem/:slug", (req, res) => {
 
 app.get("/404", (req, res) => {
     res.send("Erro 404!")
+})
+
+app.get("/categorias", (req, res) => {
+    Categoria.find().lean().then((categorias) => {
+        res.render("categorias/index.handlebars", {categorias: categorias})
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao listar as categorias");
+        res.redirect("/");
+    })
+})
+
+app.get("/categorias/:slug", (req, res) => {
+    Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+        if(categoria){
+            Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                res.render("categorias/postagens.handlebars", {postagens: postagens, categoria: categoria});
+            }).catch((err) => {
+                req.flash("error_msg", "Houve um erro ao listar os posts.");
+                res.redirect("/")
+            })
+        }
+        else{
+            req.flash("error_msg", "Esta categoria não existe");
+            res.redirect("/");
+        }
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro interno.");
+        res.redirect("/");
+    })
 })
 
 app.use('/admin', admin)
