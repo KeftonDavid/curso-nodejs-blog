518985d18cbd7215f434dc137b858341e811f7cd
KeftonDavid <keftondavid@gmail.com>
2022-08-18 22:36:58 -0300

    Validação de formulario de usuarios: correção na validação feita

--- blogapp/routes/usuario.js
+++ blogapp/routes/usuario.js
@@ -14,23 +14,27 @@ router.post("/registro", (req, res) => {
     if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
         erros.push({texto: "Nome inválido "});
     }
+
     if(!req.body.email || typeof req.body.email == undefined || req.body.email == null){
         erros.push({texto: "Email inválido "});
     }
+
     if(!req.body.senha || typeof req.body.senha == undefined || req.body.senha == null){
         erros.push({texto: "Senha inválida "});
     }
-    if(!req.body.senha.lenght < 4){
+
+    if(!req.body.senha.length < 4){
         erros.push({texto: "Senha muito pequena"});
     }
+
     if(!req.body.senha != req.body.senha2){
         erros.push({texto: "As senhas são diferentes. Tente novamente!"});
     }
-    if(erros.lenght > 0){
+    if(erros.length > 0){
         res.render("usuarios/registro.handlebars", {erros: erros})
     }
     else{
-        //em breve
+        res.send("deu certo");
     }
 })
 
