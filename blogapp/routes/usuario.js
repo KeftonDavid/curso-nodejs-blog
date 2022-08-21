3392bed11951471c8a9f990c5810f9d4e0d1a8f2
KeftonDavid <keftondavid@gmail.com>
2022-08-21 17:57:27 -0300

    Logout de sessão de usuário: nova rota funcionando e botão na navbar para encerrar sessão

--- blogapp/routes/usuario.js
+++ blogapp/routes/usuario.js
@@ -86,4 +86,11 @@ router.post("/login", (req, res, next) => {
     })(req, res, next)
 })
 
+router.get("/logout", (req, res) => {
+    req.logout((err) => {
+        req.flash("success_msg", "Deslogado com sucesso!");
+        res.redirect("/");
+    });
+})
+
 module.exports = router;
