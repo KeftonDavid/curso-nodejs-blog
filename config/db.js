188c274b7e0ab9bb8088638242c7f8b668ef9933
KeftonDavid <keftondavid@gmail.com>
2022-08-23 05:05:28 -0300

    Correção na conexão com o banco

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: `mongodb+srv://KeftonDavid:<${process.env.DATABASE_KEY}>@blogapp.ezas8am.mongodb.net/?retryWrites=true&w=majority`}
+    module.exports = {mongoURI: `mongodb+srv://KeftonDavid:${process.env.DATABASE_KEY}@blogapp.ezas8am.mongodb.net/?retryWrites=true&w=majority`}
 }
 else{
     module.exports = {mongoURI: "mongodb://localhost/blogapp"}
